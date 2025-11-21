ecba6ac448c3b9882d864b966a0723c0caeaef5b
mcollura <mirko.collura@datasensing.com>
2025-11-21 18:55:31 +0100

    Add example manager property

--- WpfAppBase/Managers/ApplicationManager.cs
+++ WpfAppBase/Managers/ApplicationManager.cs
@@ -11,12 +11,15 @@ namespace WpfAppBase.Managers
 		public static ApplicationManager Instance => _lazyInstance.Value;
 
 		public IServiceProvider ServiceProvider { get; }
+		public IExampleManager ExampleManager { get; }
 
 		private ApplicationManager()
 		{
 			ServiceProvider = new ServiceCollection()
 				.AddSingleton<IExampleManager, ExampleManager>()
 				.BuildServiceProvider();
+
+			ExampleManager = ServiceProvider.GetRequiredService<ExampleManager>();
 		}
 	}
 }
